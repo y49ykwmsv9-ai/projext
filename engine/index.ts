3afd5c2893940770387ab5850d43505517f10f0c
y49ykwmsv9-ai <y49ykwmsv9@privaterelay.appleid.com>
2026-09-21 15:49:47 -0400

    Export geographic ratio engine

--- engine/index.ts
+++ engine/index.ts
@@ -1,1 +1,1 @@
-export * from './types';export * from './simulation';export * from './commands';
+export * from './types';export * from './simulation';export * from './commands';export * from './region-ratios';
